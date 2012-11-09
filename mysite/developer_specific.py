27472575e356005221036d578141497eea1b04fb
sidor <szymon.sidor@gmail.com>
2012-11-09 20:03:29 +0000

    proper git ignore

---
+++ mysite/developer_specific.py
@@ -0,0 +1,1 @@
+PROJECT_PATH="/home/sidor/Desktop/informatyka/django/mysite"
--- mysite/settings.py
+++ mysite/settings.py
@@ -1,5 +1,7 @@
 # Django settings for mysite project.
 
+from developer_specific import *
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/sidor/Desktop/informatyka/django/mysite/main.db',                      # Or path to database file if using sqlite3.
+        'NAME': PROJECT_PATH + '/main.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -109,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/sidor/django_templates',
+    PROJECT_PATH+'/templates',
 )
 
 INSTALLED_APPS = (
